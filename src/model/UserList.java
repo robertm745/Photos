45169f01b9987e9c0c28c2259f70935230e7d028
robertm745 <rmannifield@gmail.com>
2020-11-08 21:01:01 -0500

    .

---
+++ src/model/UserList.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class UserList {
+	private ArrayList<User> users;
+	public UserList() {
+		// TODO Auto-generated constructor stub
+		this.users = new ArrayList<User>();
+		// read data
+	}
+
+}
